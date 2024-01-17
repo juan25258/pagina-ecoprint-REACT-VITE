6dda03efee4c0ac59c50dbff4491e136065634b3
juan25258 <juan25257@gmail.com>
2024-01-16 22:03:59 -0300

    bosquejo inicial  app ecoprint

---
+++ app_ecoprint/src/components/Header/header.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+
+export default function header() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      
+        <div className="container-fluid">
+          
+
+          {/* Botón para colapsar opciones en pantallas pequeñas */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Logo */}
+          <a className="navbar-brand mx-auto" href="#">
+            <img src="/ruta/al/logo.png" alt="Logo" height="30" />
+          </a>
+
+          {/* Opciones de navegación */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Opción 1
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Opción 2
+                </a>
+              </li>
+              {/* Agrega más opciones según sea necesario */}
+            </ul>
+          </div>
+
+          {/* Botones de registro y acceso */}
+          <div className="d-flex">
+            <button className="btn btn-outline-primary me-2">
+              Registrarse
+            </button>
+            <button className="btn btn-primary">Acceder</button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
