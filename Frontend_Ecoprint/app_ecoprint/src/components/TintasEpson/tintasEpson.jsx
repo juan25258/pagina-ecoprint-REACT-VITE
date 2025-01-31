5dc5e40b54233f7d3213c3faae3b3eb8298a8f60
juan25258 <juan25257@gmail.com>
2025-01-31 20:19:44 -0300

    hecha la funcion agregar al carrito en chiplessComponent

--- Frontend_Ecoprint/app_ecoprint/src/components/TintasEpson/tintasEpson.jsx
+++ Frontend_Ecoprint/app_ecoprint/src/components/TintasEpson/tintasEpson.jsx
@@ -5,84 +5,13 @@ import { useNavigate } from "react-router-dom";
 import "./tintasEpson.css";
 import 'react-toastify/dist/ReactToastify.css';
 
-/* const tintasEpsonData = [
-  {
-    imgSrc:
-      "https://scontent.fres2-1.fna.fbcdn.net/v/t39.30808-6/405388162_3490021011328292_2666548157252446078_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3635dc&_nc_ohc=rzYqfxdjfykAX-9UmvP&_nc_ht=scontent.fres2-1.fna&oh=00_AfBzrL4cNve1Tg2EdYWV-pP55B-xGNH1Cn_GMgW01aqAVA&oe=65BC2048",
-    title: "Tintas art-jet 100 mL",
-    description: "tinta negra para impresoras epson serie L.",
-    price: "$ 5000",
-  },
-  {
-    imgSrc:
-      "https://scontent.fres2-1.fna.fbcdn.net/v/t39.30808-6/405333893_3490020904661636_1937050821414450689_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=pVeHqBZzYdIAX-XDyIA&_nc_ht=scontent.fres2-1.fna&oh=00_AfBsTbJPtxrL94LJaybtS533yj30g2Z3tsNj_YrjILbnpQ&oe=65BCA130",
-    title: "Tintas art-jet 100 mL",
-    description: "tinta cian para impresoras epson serie L.",
-    price: "$ 5000",
-  },
-  {
-    imgSrc:
-      "https://scontent.fres2-1.fna.fbcdn.net/v/t39.30808-6/405387649_3490020894661637_215873478850225025_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=3635dc&_nc_ohc=KJr-aW-aSwcAX-i3eGk&_nc_oc=AQm4badIpaqZOWlstr-mlv28T8zYIvar-pG9TT2NcTs88zTkHRE_Tt5hx4T4ST_b-aA&_nc_ht=scontent.fres2-1.fna&oh=00_AfCNzq4X8cSzwrXfw8HDlzNyVwsu9dkPKc2uRkkSxOlQmQ&oe=65BABFC0",
-    title: "Tintas art-jet 100 mL",
-    description: "tinta amarilla para impresoras epson serie L.",
-    price: "$ 5000",
-  },
-  {
-    imgSrc:
-      "https://scontent.fres2-1.fna.fbcdn.net/v/t39.30808-6/405397582_3490020961328297_6614148871496758346_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_ohc=kTd_1xzNtZQAX8oKIN4&_nc_oc=AQnyoXGsrKChjyRNWk1jbGkG-10cTT4IzRSmFKCvj5kyWANZDSm1kdtwLrNtg8Ipdqg&_nc_ht=scontent.fres2-1.fna&oh=00_AfC8zdTG9jRPfnilx7GLpNMGCJrmFUn1e9lDyG4FSM-jZA&oe=65BBB5C5",
-    title: "Tintas art-jet 100 mL",
-    description: "tinta magenta para impresoras epson serie L.",
-    price: "$ 5000",
-  },
-  {
-    imgSrc:
-      "https://scontent.fres2-1.fna.fbcdn.net/v/t39.30808-6/406418774_3490020934661633_5166723865792159638_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=xCYahPBKJJcAX8EkYMr&_nc_ht=scontent.fres2-1.fna&oh=00_AfCMtRnVg4xzBxX_KZ0YAzQ8C56wirMBtqPZqw8zJ7l4_g&oe=65BAC2A7",
-    title: "Tintas art-jet 100 mL",
-    description: "tinta cian light para impresoras epson serie L.",
-    price: "$ 5000",
-  },
-  {
-    imgSrc:
-      "https://scontent.fres2-1.fna.fbcdn.net/v/t39.30808-6/406451516_3490020971328296_5666064836278898311_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=3635dc&_nc_ohc=nyjtNri8bLoAX8-T8AE&_nc_ht=scontent.fres2-1.fna&oh=00_AfCsODuoYkS8shqbstMJ7g73uUfkL7ie8QqA3r7ALovxNA&oe=65BADDDC",
-    title: "Tintas art-jet 100 mL",
-    description: "tinta magenta light para impresoras epson serie L.",
-    price: "$ 5000",
-  },
-  {
-    imgSrc:
-      "https://acdn.mitiendanube.com/stores/001/796/172/products/tinta-comercial-negro-150ml1-56a8cb012279c4d0ae16897053486997-640-0.webp",
-    title: "Tintas art-jet 150 mL",
-    description: "tinta negra para impresoras epson serie L.",
-    price: "$ 7000",
-  },
-  {
-    imgSrc:
-      "https://acdn.mitiendanube.com/stores/001/796/172/products/tinta-comercial-cian-150ml1-3a6a98b59851c9e9cf16897053488738-1024-1024.webp",
-    title: "Tintas art-jet 150 mL",
-    description: "tinta cian para impresoras epson serie L.",
-    price: "$ 7000",
-  },
-  {
-    imgSrc:
-      "https://acdn.mitiendanube.com/stores/001/796/172/products/tinta-comercial-amarillo-150ml1-af4ee76ab920b1a5da16897053487435-1024-1024.webp",
-    title: "Tintas art-jet 150 mL",
-    description: "tinta amarilla para impresoras epson serie L.",
-    price: "$ 7000",
-  },
-  {
-    imgSrc:
-      "https://acdn.mitiendanube.com/stores/001/796/172/products/tinta-comercial-magenta-150ml1-db3ddba71c4145275416897053487894-1024-1024.webp",
-    title: "Tintas art-jet 150 mL",
-    description: "tinta magenta para impresoras epson serie L.",
-    price: "$ 7000",
-  },
-]; */
+
 const tintasEpsonData = [
   {
     id: "64bff9fbd78b4c2a3d6e2b8c1",
     sku: "ARTJET-COM-100-NEGRO",
     category: "comercial",
-    imgSrc: "",
+    imgSrc: "./src/assets/tintas-artjet/tinta-comercial-negro-100ml.jpg",
     title: "Tintas art-jet 100 mL",
     description: "tinta negra para impresoras epson serie L.",
     price: 5000,
@@ -96,7 +25,7 @@ const tintasEpsonData = [
     id: "64bff9fbd78b4c2a3d6e2b8c2",
     sku: "ARTJET-COM-100-CIAN",
     category: "comercial",
-    imgSrc: "",
+    imgSrc: "./src/assets/tintas-artjet/tinta-comercial-cian-100ml (2).jpg",
     title: "Tintas art-jet 100 mL",
     description: "tinta cian para impresoras epson serie L.",
     price: 5000,
@@ -110,7 +39,7 @@ const tintasEpsonData = [
     id: "64bff9fbd78b4c2a3d6e2b8c3",
     sku: "ARTJET-COM-100-AMARILLO",
     category: "comercial",
-    imgSrc: "",
+    imgSrc: "./src/assets/tintas-artjet/tinta-comercial-amarillo-100ml.jpg.webp",
     title: "Tintas art-jet 100 mL",
     description: "tinta amarilla para impresoras epson serie L.",
     price: 5000,
@@ -124,7 +53,7 @@ const tintasEpsonData = [
     id: "64bff9fbd78b4c2a3d6e2b8c4",
     sku: "ARTJET-COM-100-MAGENTA",
     category: "comercial",
-    imgSrc: "",
+    imgSrc: "./src/assets/tintas-artjet/tinta-comercial-magenta-100ml.jpg",
     title: "Tintas art-jet 100 mL",
     description: "tinta magenta para impresoras epson serie L.",
     price: 5000,
@@ -138,7 +67,7 @@ const tintasEpsonData = [
     id: "64bff9fbd78b4c2a3d6e2b8c5",
     sku: "ARTJET-COM-100-CIAN-LIGHT",
     category: "comercial",
-    imgSrc: "",
+    imgSrc: "./src/assets/tintas-artjet/tinta-comercial-cian-claro100ml.jpg",
     title: "Tintas art-jet 100 mL",
     description: "tinta cian light para impresoras epson serie L.",
     price: 5000,
@@ -152,7 +81,7 @@ const tintasEpsonData = [
     id: "64bff9fbd78b4c2a3d6e2b8c6",
     sku: "ARTJET-COM-100-MAGENTA-LIGHT",
     category: "comercial",
-    imgSrc: "",
+    imgSrc: "./src/assets/tintas-artjet/tinta-comercial-magenta-claro100ml.jpg",
     title: "Tintas art-jet 100 mL",
     description: "tinta magenta light para impresoras epson serie L.",
     price: 5000,
